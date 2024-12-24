0c4e9e03c84583519c9c38c99ce84d3a567e5bb0
zinkevish <zinkevish77@gmail.com>
2024-12-24 19:46:33 +0300

    Update module_3_8.py

--- module_3_8.py
+++ module_3_8.py
@@ -11,12 +11,12 @@ def calculate_structure_sum(*args):
     total_sum = 0
 
     for arg in args:
-        if isinstance(arg, (int, float)):  # Если это число
+        if isinstance(arg, (int, float)):  
             total_sum += arg
-        if isinstance(arg, (list, tuple, set)):  # Если это список, кортеж или множество
-            total_sum += calculate_structure_sum(*arg)  # Рекурсия для вложенных элементов
-        if isinstance(arg, dict):  # Если это словарь
-            total_sum += calculate_structure_sum(*arg.values())  # Рекурсия для значений словаря
+        if isinstance(arg, (list, tuple, set)):  
+            total_sum += calculate_structure_sum(*arg)  
+        if isinstance(arg, dict):  
+            total_sum += calculate_structure_sum(*arg.values())  
         
 
     return total_sum
