dc7e1313d2bec6d3f25ae56894ba31dd4c40bbbe
zinkevish <zinkevish77@gmail.com>
2024-12-25 18:12:50 +0300

    Update module_3_8.py

--- module_3_8.py
+++ module_3_8.py
@@ -2,25 +2,24 @@ data_structure = [
     [1, 2, 3],
     {'a': 4, 'b': 5},
     (6, {'cube': 7, 'drum': 8}),
-    "Hello",
+    "123",
     ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
-print(data_structure, type(data_structure))
 
-def calculate_structure_sum(*args):
+def calculate_structure_sum(args):
     total_sum = 0
 
     for arg in args:
-        if isinstance(arg, (int, float)):  
+        if isinstance(arg, (int, float)):                       # Если это число
             total_sum += arg
-        if isinstance(arg, (list, tuple, set)):  
-            total_sum += calculate_structure_sum(*arg)  
-        if isinstance(arg, dict):  
-            total_sum += calculate_structure_sum(*arg.values())  
-        
+        elif isinstance(arg, (list, tuple, set)):               # Если это список, кортеж или множество
+            total_sum += calculate_structure_sum(arg)           # Рекурсия для вложенных элементов
+        elif isinstance(arg, dict):                             # Если это словарь
+            total_sum += calculate_structure_sum(arg.values())  # Рекурсия для значений словаря
+        elif isinstance (arg, str):                             # Если это строка
+            total_sum += len (arg)
 
     return total_sum
-
 
 result = calculate_structure_sum(data_structure)
 print(result)
