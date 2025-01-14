6624f3033fc1d119277f8bfc885c2c01f753f8e0
zinkevish <zinkevish77@gmail.com>
2025-01-14 19:11:01 +0300

    Update module_3_8.py

--- module_3_8.py
+++ module_3_8.py
@@ -1,9 +1,10 @@
+
 data_structure = [
-    [1, 2, 3],
-    {'a': 4, 'b': 5},
-    (6, {'cube': 7, 'drum': 8}),
-    "123",
-    ((), [{(2, 'Urban', ('Urban2', 35))}])
+[1, 2, 3],
+{'a': 4, 'b': 5},
+(6, {'cube': 7, 'drum': 8}),
+"Hello",
+((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
 
 def calculate_structure_sum(args):
